c315b279cfbd7d0017a692895f8d98e4edcda0a9
payman <paymanmolaee2@gmail.com>
2025-11-19 20:41:02 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { 
   Upload, FileText, CheckCircle, AlertTriangle, XCircle, 

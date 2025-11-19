ebeed1b06b4ced205c670def4c6f09a8f1135d34
payman <paymanmolaee2@gmail.com>
2025-11-19 20:38:25 +0000

    analytic addon

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Analytics } from "@vercel/analytics/next"
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { 
   Upload, FileText, CheckCircle, AlertTriangle, XCircle, 
